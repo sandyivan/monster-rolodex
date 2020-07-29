165a5991891393f342a9e5ba482a54f37dde9f5c
sandeepivan <zandalba@gmail.com>
2020-07-29 19:32:43 +0800

    Updates

---
+++ precache-manifest.932cb0a86fc6637e4e842d65e0116f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000bae39c626cfa6085865b553ab8a33",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bd9ec8f54ba50d8a40e3",
+    "url": "/monster-rolodex/static/css/main.a48eb76b.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monster-rolodex/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd9ec8f54ba50d8a40e3",
+    "url": "/monster-rolodex/static/js/main.c784b387.chunk.js"
+  },
+  {
+    "revision": "3d5dc42248b87f1c09c5",
+    "url": "/monster-rolodex/static/js/runtime-main.22ce2adb.js"
+  }
+]);
